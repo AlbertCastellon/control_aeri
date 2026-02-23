223ae6f560b72455c8da3c26dbd47d9d1a2e98f5
Pepe Sufragio <albertkstyon@gmail.com>
2026-02-23 17:04:27 +0100

    first commit

---
+++ control_aeri.java
@@ -0,0 +1,10 @@
+public class control_aeri {
+    public static void main(String[] args) {
+        control_aeri p = new control_aeri();
+        p.principal();
+    }
+    public void principal() {
+        
+    }
+    
+}
---
+++ planes.java
@@ -0,0 +1,42 @@
+public class planes {
+    private String matricula;
+    private int autonomy;
+    private int posX;
+    private int posY;
+    private int capacity;
+    private pilot pilots;
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public int getAutonomy() {
+        return autonomy;
+    }
+    public void setAutonomy(int autonomy) {
+        this.autonomy = autonomy;
+    }
+    public int getPosX() {
+        return posX;
+    }
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+    public int getPosY() {
+        return posY;
+    }
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+
+}
